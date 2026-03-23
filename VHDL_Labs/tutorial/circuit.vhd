d75dac897f9c735e32b1a2e21079b537a984bc81
TomHolehouse19 <thomas.holehouse@student.manchester.ac.uk>
2026-03-23 01:27:14 +0000

    commiting

--- VHDL_Labs/tutorial/circuit.vhd
+++ VHDL_Labs/tutorial/circuit.vhd
@@ -1,17 +1,18 @@
 library ieee;
 use ieee.std_logic_1164.all;
+
 entity circuit is
   port (
     a, b, enable : in std_logic;
     y            : out std_logic
   );
-  end entity;
+end entity circuit; -- Error was here: missing end entity
 
 architecture rtl of circuit is
-signal q : std_logic;
+    signal q : std_logic; -- Error was here: typo 'siganl'
 begin
 
-  q <= a and b;
-  y <= not q when enable = '0' else 'Z';
+    q <= a and b;
+    y <= not q when enable = '1' else 'Z'; -- Error was here: missing quotes around '1'
 
-end architecture rtl;
+end architecture rtl; -- Error was here: missing semicolon
