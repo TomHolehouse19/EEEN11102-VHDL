ece22598e83c313b399eb1ed2c236d2f2adab9c6
TomHolehouse19 <thomas.holehouse@student.manchester.ac.uk>
2026-03-23 01:25:13 +0000

    commiting

---
+++ VHDL_Labs/tutorial/circuit.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+entity circuit is
+  port (
+    a, b, enable : in std_logic;
+    y            : out std_logic
+  );
+  end entity;
+
+architecture rtl of circuit is
+signal q : std_logic;
+begin
+
+  q <= a and b;
+  y <= not q when enable = '0' else 'Z';
+
+end architecture rtl;
